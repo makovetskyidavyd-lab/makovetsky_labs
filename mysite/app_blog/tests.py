2f4f46bcb0a1ba5fc2eb8e26ccae5d3b42d77572
loberto <csgofan2005@gmail.com>
2025-12-07 19:51:03 +0200

    Lab 4

--- mysite/app_blog/tests.py
+++ mysite/app_blog/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import HomePageView, ArticleList
 
-# Create your tests here.
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)  # Було assertEquals
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEqual(view.func.view_class, HomePageView) # Було assertEquals
+
+class CategoryTest(TestCase):
+    def test_category_view_status_code(self):
+        url = reverse('articles-category-list', args=('name',))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200) # Було assertEquals
+
+class ArticlesListTest(TestCase):
+    def test_articles_list_view_status_code(self):
+        url = reverse('articles-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200) # Було assertEquals
+    
+    def test_articles_list_url_resolves_view(self):
+        view = resolve('/articles')
+        self.assertEqual(view.func.view_class, ArticleList) # Було assertEquals
